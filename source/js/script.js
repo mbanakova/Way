295495c2e7cce8c2112b1f73a213517011fd77bd
Маша <mbanakova@yandex.ru>
2020-12-08 18:51:18 +0300

    init commit

---
+++ source/js/script.js
@@ -0,0 +1,85 @@
+'use strict';
+
+// Бургер-меню
+var nav = document.querySelector('.nav');
+var navList = nav.querySelector('.nav__list');
+var navItems = nav.querySelectorAll('.nav__item');
+var navLinks = nav.querySelectorAll('.nav__link');
+var logo = nav.querySelector('.nav__logo');
+var burger = nav.querySelector('.nav__burger');
+
+nav.classList.remove('nav--nojs');
+nav.classList.remove('header__nav--nojs');
+navList.classList.remove('nav__list--nojs');
+
+for (var i = 0; i < navItems.length; i++) {
+  navItems[i].classList.remove('nav__item--nojs');
+}
+
+for (var k = 0; k < navLinks.length; k++) {
+  navLinks[k].classList.remove('nav__link--nojs');
+}
+
+burger.classList.remove('nav__burger--nojs');
+
+burger.addEventListener('click', function () {
+  if (burger.classList.contains('nav__burger--cross')) {
+    burger.classList.remove('nav__burger--cross');
+    nav.classList.remove('header__nav--js');
+    nav.classList.remove('nav--js');
+    logo.classList.remove('nav__logo--js');
+    navList.classList.remove('nav__list--js');
+
+  } else {
+    burger.classList.add('nav__burger--cross');
+    nav.classList.add('header__nav--js');
+    nav.classList.add('nav--js');
+    logo.classList.add('nav__logo--js');
+    navList.classList.add('nav__list--js');
+  }
+});
+
+// Табы
+document.querySelectorAll('.tabs__country').forEach((item) =>
+  item.addEventListener('click', function (elem) {
+    elem.preventDefault();
+    var id = elem.target.getAttribute('href').replace('#', '');
+
+    document.querySelectorAll('.tabs__info').forEach(
+      (child) => child.classList.remove('tabs__info--active'));
+
+    document.getElementById(id).classList.add('tabs__info--active')
+  }));
+
+// Купить тур
+
+var trigger = document.querySelectorAll('.tariffs__button');
+var purchase = document.querySelector('.purchase');
+
+trigger.forEach((item) =>
+  item.addEventListener('click', function (elem) {
+    elem.preventDefault();
+    purchase.classList.add('body__modal--active');
+}));
+
+var closePurchase = document.querySelector('.purchase__close');
+
+closePurchase.addEventListener('click', function (elem) {
+  elem.preventDefault();
+  purchase.classList.remove('body__modal--active');
+})
+
+// "Отзыв"
+var success = document.querySelector('.success');
+var closeSuccess = document.querySelector('.success__close');
+var formSubmit = document.querySelector('.form__submit');
+
+formSubmit.addEventListener('click', function (elem) {
+  elem.preventDefault();
+  success.classList.add('body__modal--active');
+});
+
+closeSuccess.addEventListener('click', function (elem) {
+  elem.preventDefault();
+  success.classList.remove('body__modal--active');
+})
